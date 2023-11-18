dca15550022179a031d173a0674a6e92c0573f75
PedroPianna <pedro.pianna@icloud.com>
2023-11-18 19:39:05 -0300

    adicionar função de gráfico  barras com proporção

--- helper.py
+++ helper.py
@@ -2,15 +2,33 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 def grafico_barras_quantidade(coluna: pd.Series, titulo = "Número de bolsistas"):
-    """Essa função recebe um objeto pd.Series e uma legenda. Retorna elementos para plotar um gráfico de barras"""
+    """Essa função recebe um objeto pd.Series e, opcionalmente, um título. Pega os atributos únicos da variável, bem como seus respectivos valores e os usa para fazer um gráfico de barras. Retorna um fig, ax para serem plotados"""
     fig, ax = plt.subplots()
 
     variavel = coluna.unique()
     observacoes = []
     for num, tipo in enumerate(variavel):
         observacoes.append(coluna.value_counts()[tipo])
+        
     bar_colors = ['tab:red', 'tab:green']
+    ax.bar(variavel, observacoes, color=bar_colors)
+    
+    ax.set_ylabel('número de bolsistas')
+    ax.set_title(titulo)
 
+    return fig, ax
+
+def grafico_barras_proporcao(coluna: pd.Series, titulo = "Número de bolsistas"):
+    """Essa função recebe um objeto pd.Series e, opcionalmente, um título. Pega os atributos únicos da variável, bem como seus respectivos valores e os usa para fazer um gráfico de barras com as proporções de cada entrada. Retorna um fig, ax para serem plotados"""
+    fig, ax = plt.subplots()
+
+    variavel = coluna.unique()
+    total = coluna.count()
+    observacoes = []
+    for num, tipo in enumerate(variavel):
+        observacoes.append(coluna.value_counts()[tipo] / total)
+
+    bar_colors = ['tab:red', 'tab:green']
     ax.bar(variavel, observacoes, color=bar_colors)
     
     ax.set_ylabel('número de bolsistas')
